3ed68550049cc0d551b9706df7d04d87341a241f
Gustavo Leite <gustavo.jog@hotmail.com>
2025-07-07 19:48:46 -0300

    feat: setup project

---
+++ src/main/java/com/talkevents/jpa/entities/Event.java
@@ -0,0 +1,25 @@
+package com.talkevents.jpa.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class Event implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private LocalDateTime date;
+}
